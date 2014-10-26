30397f0abad7561c529d5e16e01f2804b81e4ed9
Hieu Dang <hoanghieu3690@gmail.com>
2014-10-26 11:29:26 +0700

    finish #2 add helpers for keyup and keydown, blur event. Add readme.md

---
+++ README.md
@@ -0,0 +1,17 @@
+## Notes Introduction
+
+Create a simple notes synchronizing through multiple devices
+
+### Installation 
+
+> git clone git@github.com:golden-illusion/notes.git
+> cd notes
+> meteor update
+> meteor
+
+If everything ok, this app is ready on `localhost:3000`
+
+### Features
+
+1. CRUD notes
+2. manage by categories
--- client/notes.js
+++ client/notes.js
@@ -4,6 +4,27 @@ Meteor.subscribe("notes");
 
 Session.setDefault("editing_note", null)
 
+//helpers
+var okCancelEvents = function (selector, callbacks) {
+  var ok = callbacks.ok || function () {};
+  var cancel = callbacks.cancel || function () {};
+
+  var events = {};
+  events['keyup '+ selector + ', keydown ' + selector + ', focusout ' +selector] =
+    function (evt) {
+      if (evt.type === "keydown" && evt.which === 27) {
+        cancel.call(this, evt);
+      }
+      else if (evt.type === "keyup" && evt.which === 13 || evt.type === "focusout") {
+        var value = String(evt.target.value || "");
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+  return events;
+};
 
 //temlates
 Template.noteList.helpers({
